565c19b506b0cb54662debe207442bf06c80e872
rrauf8178 <rrauf8178@gmail.com>
2024-04-25 18:11:26 +0400

    add: backend configuration initially

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+//Files
+
+import connectDB from "./config/db.js";
+
+//Configurations
+
+dotenv.config();
+
+//Connect to MongoDB
+
+connectDB();
+
+const app = express();
+
+//Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({ expended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+//Routes
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
