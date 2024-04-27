14bdcec15f45bb79b8f329a486c84ec0f8a508b0
rrauf8178 <rrauf8178@gmail.com>
2024-04-28 02:15:00 +0400

    add: loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,7 @@
-
-
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-teal-500 border-opacity-50"></div>
+  );
+};
 
-export default Loader
+export default Loader;
