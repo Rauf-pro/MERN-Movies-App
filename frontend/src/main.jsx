93ef80bcb96e043f2c4c47dec481dc0e291ff297
rrauf8178 <rrauf8178@gmail.com>
2024-04-27 22:38:10 +0400

    add: auth reducer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,23 @@
-
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowsersRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// Auth
+
+// Restricted
+
+const router = createBrowsersRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </Provider>
+);
