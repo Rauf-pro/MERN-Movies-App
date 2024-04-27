3f7276fae42081199c5cee567b3fa4808811810a
rrauf8178 <rrauf8178@gmail.com>
2024-04-27 21:06:36 +0400

    add: getcurrentuserprofile and updateuserprofile contorllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,9 +80,60 @@ const logCurrentUser = asyncHandler(async (req, res) => {
 });
 
 const getAllUsers = asyncHandler(async (req, res) => {
-    const users = await User.find({});
+  const users = await User.find({});
 
-    res.json(users)
-})
+  res.json(users);
+});
 
-export { createUser, loginUser, logCurrentUser, getAllUsers };
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+const updateCurrentUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  }else{
+    res.status(404);
+    throw new Error("User not found");
+  
+  }
+});
+
+export {
+  createUser,
+  loginUser,
+  logCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 
 // controllers
 
-import { createUser, loginUser, logCurrentUser , getAllUsers} from "../controllers/userController.js";
+import { createUser, loginUser, logCurrentUser , getAllUsers, getCurrentUserProfile, updateCurrentUserProfile} from "../controllers/userController.js";
 
 //middlewares
 
@@ -13,6 +13,7 @@ const router = express.Router();
 router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logCurrentUser);
+router.route("/profile").get(authenticate, getCurrentUserProfile).put(authenticate, updateCurrentUserProfile);
 
 
 export default router;
