a08a9f9b21de976ed4db21e42c72d5504d49b11d
rrauf8178 <rrauf8178@gmail.com>
2024-05-02 18:27:55 +0400

    add: requests for genres

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import path from "path";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import genreRoutes from "./routes/genreRoutes.js";
 
 //Configurations
 
@@ -29,6 +30,7 @@ const PORT = process.env.PORT || 3000;
 //Routes
 
 app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/genre", genreRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
