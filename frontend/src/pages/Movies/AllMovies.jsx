b403223218298a611d1ae758d109951a2523e59a
rrauf8178 <rrauf8178@gmail.com>
2024-05-06 18:15:51 +0400

    add: moviesSlice for all movies show

---
+++ frontend/src/pages/Movies/AllMovies.jsx
@@ -0,0 +1,19 @@
+import { useGetAllMoviesQuery } from "../../redux/api/movies";
+import { useFetchGenresQuery } from "../../redux/api/genre";
+import {
+  useGetNewMoviesQuery,
+  useGetTopMoviesQuery,
+  useGetRandomMoviesQuery,
+} from "../../redux/api/movies";
+import MovieCard from "./MovieCard";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import banner from "../../assets/banner.jpg";
+
+const AllMovies = () => {
+  return (
+    <div>AllMovies</div>
+  )
+}
+
+export default AllMovies
