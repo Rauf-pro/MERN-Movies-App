6303981b14237ce2ffc46ddbad296f63686251b9
rrauf8178 <rrauf8178@gmail.com>
2024-05-06 16:11:16 +0400

    add: image upload conroller and routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import path from "path";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 //Files
 
@@ -33,6 +34,10 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/genre", genreRoutes);
 app.use("/api/v1/movies", moviesRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
